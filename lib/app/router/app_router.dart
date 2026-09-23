0ecc604e79f8296123f1d0d807b0097f196b95cd
Yehya Elsayed <100237058+Yehyaelzaeem@users.noreply.github.com>
2026-09-23 19:17:29 +0300

    refactor: Rename company-related files and update imports for consistency

--- lib/app/router/app_router.dart
+++ lib/app/router/app_router.dart
@@ -4,16 +4,7 @@ import 'package:Silink/features/account/presentation/account_screen.dart';
 import 'package:Silink/features/auth/data/models/user_model.dart';
 import 'package:Silink/features/clients/models/client_model.dart';
 import 'package:Silink/features/clients/presentation/client_details_screen.dart';
-import 'package:Silink/features/company/presentation/company_brand_screen.dart';
-import 'package:Silink/features/company/presentation/company_cards_screen.dart';
-import 'package:Silink/features/company/presentation/company_catalog_screen.dart';
-import 'package:Silink/features/company/presentation/company_dashboard_screen.dart';
-import 'package:Silink/features/company/presentation/company_edit_screen.dart';
-import 'package:Silink/features/company/presentation/company_employees_screen.dart';
-import 'package:Silink/features/company/presentation/company_publish_screen.dart';
-import 'package:Silink/features/company/presentation/company_qr_screen.dart';
-import 'package:Silink/features/company/presentation/company_setup_screen.dart';
-import 'package:Silink/features/company/presentation/company_success_screen.dart';
+
 import 'package:Silink/features/help/presentation/help_screen.dart';
 import 'package:Silink/features/notifications/presentation/notifications_screen.dart';
 import 'package:Silink/features/privacy/presentation/privacy_screen.dart';
@@ -41,6 +32,16 @@ import '../../features/auth/presentation/login_screen.dart';
 import '../../features/auth/presentation/otp_screen.dart';
 import '../../features/auth/presentation/personal_register_screen.dart';
 import '../../features/auth/presentation/register_screen.dart';
+import '../../features/company/company_card/presentation/company_cards_screen.dart';
+import '../../features/company/company_card/presentation/company_publish_screen.dart';
+import '../../features/company/company_card/presentation/company_qr_screen.dart';
+import '../../features/company/company_catalog/presentation/company_catalog_screen.dart';
+import '../../features/company/company_dashboard/presentation/company_dashboard_screen.dart';
+import '../../features/company/company_profile_completion/presentation/company_setup_screen.dart';
+import '../../features/company/company_profile_completion/presentation/company_success_screen.dart';
+import '../../features/company/company_settings/presentation/company_brand_screen.dart';
+import '../../features/company/company_settings/presentation/company_edit_screen.dart';
+import '../../features/company/company_team/presentation/company_employees_screen.dart';
 import '../../features/layout/presentation/layout_screen.dart';
 import '../../features/onboarding/presentation/on_boarding_screen.dart';
 import '../../features/splash/presentation/splash_screen.dart';
@@ -163,8 +164,6 @@ class RouteGenerator {
         ));
       case Routes.companySetup:
         return _pageRoute(const CompanySetupScreen());
-      case Routes.companySuccess:
-        return _pageRoute(const CompanySuccessScreen());
       case Routes.companyDashboard:
         return _pageRoute(const CompanyDashboardScreen());
       case Routes.companyCards:
@@ -181,6 +180,18 @@ class RouteGenerator {
         return _pageRoute(const CompanyPublishScreen());
       case Routes.companyEdit:
         return _pageRoute(const CompanyEditScreen());
+      case Routes.companySuccess:
+        return _pageRoute(CompanySuccessScreen(
+          companyName: arguments?['companyName'] as String? ?? '',
+          industry: arguments?['industry'] as String? ?? '',
+          subIndustry: arguments?['subIndustry'] as String? ?? '',
+          size: arguments?['size'] as String? ?? '',
+          city: arguments?['city'] as String? ?? '',
+          phone: arguments?['phone'] as String? ?? '',
+          email: arguments?['email'] as String? ?? '',
+          website: arguments?['website'] as String? ?? '',
+          bio: arguments?['bio'] as String? ?? '',
+        ));
       default:
         return _pageRoute(const _UndefinedScreen());
     }
